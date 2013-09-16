351a6a5294a74a8781b4dc8477c08e60cc3d83a4
Kévin Gomez <geek63@gmail.com>
2013-09-16 13:10:39 +0100

    Fix: updated tests

--- Tests/SmsSender/LoggableSmsSenderTest.php
+++ Tests/SmsSender/LoggableSmsSenderTest.php
@@ -3,7 +3,6 @@
 namespace KPhoen\SmsSenderBundle\Tests\SmsSender;
 
 use SmsSender\Provider\DummyProvider;
-use SmsSender\Result\ResultInterface;
 
 use KPhoen\SmsSenderBundle\SmsSender\LoggableSmsSender;
 
@@ -14,33 +13,61 @@ class LoggableSmsSenderTest extends \PHPUnit_Framework_TestCase
 {
     public function testSendLogsData()
     {
+        $provider = $this->getMock('\SmsSender\Provider\DummyProvider');
+        $result = $this->getMock('\SmsSender\Result\ResultInterface');
+
+        // setup the sender
+        $sender = $this->getMock('\SmsSender\SmsSender');
+        $sender->expects($this->once())
+            ->method('getProvider')
+            ->will($this->returnValue($provider));
+        $sender->expects($this->once())
+            ->method('send')
+            ->with(
+                $this->equalTo('0642424242'),
+                $this->equalTo('content'),
+                $this->equalTo('originator')
+            )
+            ->will($this->returnValue($result));
+
         // setup the logger
         $logger = $this->getMock('\KPhoen\SmsSenderBundle\Logger\SmsSenderLogger');
         $logger->expects($this->once())
             ->method('logMessage')
             ->with(
-                $this->callback(function($result) {
-                    return $result instanceof ResultInterface;
-                }),
+                $this->equalTo($result),                // the exact result returned by the sender
                 $this->greaterThan(0),                  // duration
                 $this->stringContains('DummyProvider')  // provider className
             );
 
         // setup the sender
-        $sender = new LoggableSmsSender($logger);
-        $sender->registerProvider(new DummyProvider());
+        $sender = new LoggableSmsSender($sender, $logger);
 
         // and launch the test
-        $result = $sender->send('0642424242', 'content', 'originator');
+        $senderResult = $sender->send('0642424242', 'content', 'originator');
+        $this->assertSame($result, $senderResult);
     }
 
     public function testSendWithNoLogger()
     {
-        // setup the sender
-        $sender = new LoggableSmsSender();
-        $sender->registerProvider(new DummyProvider());
+        $result = $this->getMock('\SmsSender\Result\ResultInterface');
 
-        $result = $sender->send('0642424242', 'content', 'originator');
-        $this->assertTrue(true, 'No error is raised');
+        // setup the sender
+        $sender = $this->getMock('\SmsSender\SmsSender');
+        $sender->expects($this->once())
+            ->method('send')
+            ->with(
+                $this->equalTo('0642424242'),
+                $this->equalTo('content'),
+                $this->equalTo('originator')
+            )
+            ->will($this->returnValue($result));
+
+        // setup the sender
+        $sender = new LoggableSmsSender($sender);
+
+        // and launch the test
+        $senderResult = $sender->send('0642424242', 'content', 'originator');
+        $this->assertSame($result, $senderResult);
     }
 }
